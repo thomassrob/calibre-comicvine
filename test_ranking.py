77cb086d13c96fa5d42c87a00aa40fb36dabfe52
Chris Fairbanks <cfairbanks@etsy.com>
2016-08-06 19:15:10 -0400

    more unit tests for ranking.score_title

--- test_ranking.py
+++ test_ranking.py
@@ -7,15 +7,43 @@ import ranking
 
 class TestRanking(unittest.TestCase):
   def test_score_title(self):
-    metadata = type('Metadata',
-                    (object,),
-                    {
-                      'series': 'Dogville',
-                      'series_index': 2.0,
-                      'pubdate': type('Date', (object,), {'year': 2000}),
-                      'comments': '',
-                    })
+    result = ranking.score_title(metadata=mock_metadata('Dogville', 2.0),
+                                 title='Dogville #2',
+                                 title_tokens=['dogville'])
+    self.assertEqual(29.0, result)
 
-    self.assertEqual(29.0, ranking.score_title(metadata,
-                                               title='Dogville #2',
-                                               title_tokens=['dogville']))
+  def test_score_title_with_matching_year(self):
+    result = ranking.score_title(metadata=mock_metadata('Dogville', 2.0, 2010),
+                                 title='Dogville #2 (2010)',
+                                 title_tokens=['dogville'])
+    self.assertEqual(31.0, result)
+
+  def test_score_title_with_mismatching_year(self):
+    result = ranking.score_title(metadata=mock_metadata('Dogville', 2.0, 2010),
+                                 title='Dogville #2 (2014)',
+                                 title_tokens=['dogville'])
+    self.assertEqual(35.0, result)
+
+  def test_score_title_with_extra_tokens(self):
+    result = ranking.score_title(metadata=mock_metadata('Dogville', 2.0, 2010),
+                                 title='Dogville #2 (2014)',
+                                 title_tokens=['dogville', 'awakening'])
+    self.assertEqual(65.0, result)
+
+  def test_score_title_mismatched_issue_number(self):
+    result = ranking.score_title(metadata=mock_metadata('Dogville', 2.0),
+                                 title='Dogville #5',
+                                 issue_number=5,
+                                 title_tokens=['dogville'])
+    self.assertEqual(87.0, result)
+
+
+def mock_metadata(series, series_index, year=2000, comments=''):
+  return type('Metadata',
+              (object,),
+              {
+                'series': series,
+                'series_index': series_index,
+                'pubdate': type('Date', (object,), {'year': year}),
+                'comments': comments,
+              })
