93aaf73a895013a7418ec2a47803028cccf58f26
Chris Fairbanks <cfairbanks@etsy.com>
2016-08-17 13:55:54 -0700

    more unit tests

--- test_parser.py
+++ test_parser.py
@@ -17,6 +17,18 @@ class TestParser(unittest.TestCase):
             'Magnus, Robot Fighter 01 (2010) (two covers) (Minutemen-DTs)',
             'Magnus, Robot Fighter ',
             '1')
+        self.run_normalised_title_test(
+            'Spider-Man 003.1 (2010) (extra stuff)',
+            'Spider-Man ',
+            '3.1')
+        self.run_normalised_title_test(
+            'Buffy Season 10 015 (2015) (Digital) (Cypher 2.0-Empire)',
+            'Buffy Season 10 ',
+            '15')
+        # self.run_normalised_title_test(
+        #    'Buffy the Vampire Slayer Season 10 v01 - New Rules (2014) (digital) (The Magicians-Empire)',
+        #    'Buffy the Vampire Slayer Season 10 ',
+        #    '15')
 
     def run_normalised_title_test(self,
                                   input_title,
