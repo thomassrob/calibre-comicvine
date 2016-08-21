00817a0f166f7eb405506e7d159d8d1bbc3a454c
Chris Fairbanks <cfairbanks@etsy.com>
2016-08-21 13:34:06 -0400

    add integration tests for list of issue titles

--- test_plugin.py
+++ test_plugin.py
@@ -7,6 +7,21 @@ from calibre.ebooks.metadata.sources.test import (test_identify_plugin,
 from source import Comicvine
 
 
+class TestFileList(unittest.TestCase):
+    def test_list(self):
+        f = open("test/passing_titles.txt")
+        lines = f.readlines()
+        f.close()
+
+        lines = [line.replace("\n", "") for line in lines]
+        lines = [line.split('|') for line in lines]
+
+        tuples = [({'title': line[1]}, [comicvine_id_test(line[0])])
+                  for line in lines]
+
+        test_identify_plugin(Comicvine.name, tuples)
+
+
 class TestPlugin(unittest.TestCase):
     def test_title_and_single_author_match(self):
         test_identify_plugin(Comicvine.name, [(
@@ -25,8 +40,8 @@ class TestPlugin(unittest.TestCase):
                      'Clem Robins', 'Glenn Fabry', 'Julie Rottenberg']),
                 series_test('Preacher Special: The Story of You-Know-Who',
                             1.0),
-                self.comicvine_id_test('105747'),
-                self.comicvine_volume_id_test('18059'),
+                comicvine_id_test('105747'),
+                comicvine_volume_id_test('18059'),
             ]
         )])
 
@@ -47,8 +62,8 @@ class TestPlugin(unittest.TestCase):
                      'Clem Robins', 'Glenn Fabry', 'Julie Rottenberg']),
                 series_test('Preacher Special: The Story of You-Know-Who',
                             1.0),
-                self.comicvine_id_test('105747'),
-                self.comicvine_volume_id_test('18059'),
+                comicvine_id_test('105747'),
+                comicvine_volume_id_test('18059'),
             ]
         )])
 
@@ -72,8 +87,8 @@ class TestPlugin(unittest.TestCase):
                 series_test(
                     'Preacher Special: The Story of You-Know-Who',
                     1.0),
-                self.comicvine_id_test('105747'),
-                self.comicvine_volume_id_test('18059'),
+                comicvine_id_test('105747'),
+                comicvine_volume_id_test('18059'),
             ]
         )])
 
@@ -98,38 +113,37 @@ class TestPlugin(unittest.TestCase):
                 series_test(
                     'Preacher Special: The Story of You-Know-Who',
                     1.0),
-                self.comicvine_id_test('105747'),
-                self.comicvine_volume_id_test('18059'),
+                comicvine_id_test('105747'),
+                comicvine_volume_id_test('18059'),
             ]
         )])
 
-    def comicvine_id_test(self, expected):
-        """Build a test function to assert comicvine ID."""
 
-        def test(result):
-            """Ensure that the metadata instance contains the expected data."""
-            if result.identifiers:
-                result = result.identifiers.get('comicvine')
-            else:
-                result = None
+def comicvine_id_test(expected):
+    """Build a test function to assert comicvine ID."""
 
-            self.assertEqual(expected, result, 'ID test failed for comicvine')
-            return True
+    def test(metadata):
+        """Ensure that the metadata instance contains the expected data."""
+        if metadata.identifiers:
+            result = metadata.identifiers.get('comicvine')
+        else:
+            result = None
 
-        return test
+        return result and (expected == result)
 
-    def comicvine_volume_id_test(self, expected):
-        """Build a test function to assert comicvine volume ID."""
+    return test
 
-        def test(result):
-            """Ensure that the metadata instance contains the expected data."""
-            if result.identifiers:
-                result = result.identifiers.get('comicvine-volume')
-            else:
-                result = None
 
-            self.assertEqual(expected, result,
-                             'ID test failed for comicvine-volume')
-            return True
+def comicvine_volume_id_test(expected):
+    """Build a test function to assert comicvine volume ID."""
 
-        return test
+    def test(metadata):
+        """Ensure that the metadata instance contains the expected data."""
+        if metadata.identifiers:
+            result = metadata.identifiers.get('comicvine-volume')
+        else:
+            result = None
+
+        return result and (expected == result)
+
+    return test
